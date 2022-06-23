27908f52d3666e342ca195b1e6c704e4978d30c0
theerudito <erudito.tv@gmail.com>
2022-06-23 14:12:08 -0500

    v1

--- Router/Routes.js
+++ Router/Routes.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
   CreateComputer,
   GetAllComputers,
+  GetOneComputer,
 } from "../Controls/ComputerControlers.js";
 const router = express.Router();
 
 router.get("/", GetAllComputers);
-router.get("/:id");
+router.get("/:id", GetOneComputer);
 router.post("/", CreateComputer);
 
 export default router;
