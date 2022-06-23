057b5ab406d79742b22d579a9189eb63993c4ced
theerudito <erudito.tv@gmail.com>
2022-06-23 14:10:32 -0500

    v1

--- Controls/ComputerControlers.js
+++ Controls/ComputerControlers.js
@@ -12,15 +12,27 @@ export const GetAllComputers = async (req, res) => {
 
 //===============GET ALL COMPUTERS================================================================
 
+//===============GET ONE COMPUTERS================================================================
+export const GetOneComputer = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Computers.findById({ _id: id }).then((computer) => {
+      res.status(200).json(computer);
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
 
+//===============GET ONE COMPUTERS================================================================
 
 //===============CREATE COMPUTER==================================================================
 export const CreateComputer = async (req, res) => {
   try {
     await Computers.create(req.body);
     res.status(200).json({
-      'message' : 'Computer created successfully'
-    })
+      message: "Computer created successfully",
+    });
   } catch (error) {
     res.json({ message: error.message });
   }
